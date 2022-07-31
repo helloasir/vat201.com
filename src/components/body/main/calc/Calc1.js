8e0f49f0b653322c78a21d01d501878c9c61631c
helloasir <helloasir@yahoo.com>
2022-07-31 14:06:16 +0400

    calc function commented

--- src/components/body/main/calc/Calc1.js
+++ src/components/body/main/calc/Calc1.js
@@ -1,29 +1,28 @@
-import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
-function xParseFloat(x){var amount=x.replace(',','.');amount=amount.replace(/[^0-9.]/,'');if(amount===''){return false;}else{return parseFloat(amount);}};
-function getAmount(){var amount=document.getElementById('amount').value;return xParseFloat(amount);};
-function getVat(){var amount=document.getElementById('vatper').value;return xParseFloat(amount);};
-function getOperation(){return document.getElementById('inc').checked?'Include':'Exclude';};
-function calculatorSubmit()
-{var amount=getAmount();if(amount===false||isNaN(amount)||!isFinite(amount)){return false;}
-var vat=getVat();if(vat===false||isNaN(vat)||!isFinite(vat)){return false;}
-var operation=getOperation();var result;if(operation==='Include'){result=amount-amount/(1+vat/100);}else if(operation==='Exclude'){result=amount*(1+vat/100);}
-addResults(amount,vat,operation,result);};
+// function xParseFloat(x){var amount=x.replace(',','.');amount=amount.replace(/[^0-9.]/,'');if(amount===''){return false;}else{return parseFloat(amount);}};
+// function getAmount(){var amount=document.getElementById('amount').value;return xParseFloat(amount);};
+// function getVat(){var amount=document.getElementById('vatper').value;return xParseFloat(amount);};
+// function getOperation(){return document.getElementById('inc').checked?'Include':'Exclude';};
+// function calculatorSubmit()
+// {var amount=getAmount();if(amount===false||isNaN(amount)||!isFinite(amount)){return false;}
+// var vat=getVat();if(vat===false||isNaN(vat)||!isFinite(vat)){return false;}
+// var operation=getOperation();var result;if(operation==='Include'){result=amount-amount/(1+vat/100);}else if(operation==='Exclude'){result=amount*(1+vat/100);}
+// addResults(amount,vat,operation,result);};
 
-function toCurrencyString(x){return(Math.round(x*100)/100).toFixed(2)}
-function resultBlock(caption,value){return '<div className="result-block">'+
-caption+'<br/>'+value+
-'</div>'}
-function addResults(amount,vat,operation,result){amount=toCurrencyString(amount);vat=toCurrencyString(vat);result=toCurrencyString(result);var html='<div className="result clearfix alert-primary" >'+
-resultBlock('Amount:',amount)+
-resultBlock('VAT %:',vat)+
-resultBlock('Operation:',operation)+
-(operation==='Exclude'?resultBlock('<b className="text-danger">'+'VAT Excluded:',toCurrencyString(parseFloat(result)-parseFloat(amount))+'</b>')+resultBlock('<b className="text-danger">'+'Gross amount:',result+'</b>'):resultBlock('<b className="text-dark">'+'VAT Included:',result+'</b>')+resultBlock('<b className="text-dark">'+'Net amount:',toCurrencyString(parseFloat(amount)-parseFloat(result))+'</b>'))+
-'</div>';var innerHTML=document.getElementById('results').innerHTML;innerHTML=html+innerHTML;document.getElementById('results').innerHTML=innerHTML;return true;}
+// function toCurrencyString(x){return(Math.round(x*100)/100).toFixed(2)}
+// function resultBlock(caption,value){return '<div className="result-block">'+
+// caption+'<br/>'+value+
+// '</div>'}
+// function addResults(amount,vat,operation,result){amount=toCurrencyString(amount);vat=toCurrencyString(vat);result=toCurrencyString(result);var html='<div className="result clearfix alert-primary" >'+
+// resultBlock('Amount:',amount)+
+// resultBlock('VAT %:',vat)+
+// resultBlock('Operation:',operation)+
+// (operation==='Exclude'?resultBlock('<b className="text-danger">'+'VAT Excluded:',toCurrencyString(parseFloat(result)-parseFloat(amount))+'</b>')+resultBlock('<b className="text-danger">'+'Gross amount:',result+'</b>'):resultBlock('<b className="text-dark">'+'VAT Included:',result+'</b>')+resultBlock('<b className="text-dark">'+'Net amount:',toCurrencyString(parseFloat(amount)-parseFloat(result))+'</b>'))+
+// '</div>';var innerHTML=document.getElementById('results').innerHTML;innerHTML=html+innerHTML;document.getElementById('results').innerHTML=innerHTML;return true;}
 
 
 function Calc1() {
